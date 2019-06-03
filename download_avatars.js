716cdf8dea09c2a2f4f24bd1003b11e8042cf6e0
Nick <nbjenvey@gmail.com>
2019-06-03 21:23:12 +0000

    Didn't commit my secrets

---
+++ download_avatars.js
@@ -0,0 +1,23 @@
+var request = require('request');
+var GITHUB_TOKEN = require('./secrets');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
