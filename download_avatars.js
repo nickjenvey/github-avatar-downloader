766edf169a309b3f18da6e4750a45c67f60c405e
Nick <nbjenvey@gmail.com>
2019-06-03 23:24:11 +0000

    Connected avatar url getter with avatar downloader function

--- download_avatars.js
+++ download_avatars.js
@@ -1,33 +1,31 @@
 var request = require("request");
 var fs = require("fs");
-// var GITHUB_TOKEN = require("./secrets").GITHUB_TOKEN;
+var GITHUB_TOKEN = require("./secrets").GITHUB_TOKEN;
 
-// console.log("Welcome to the GitHub Avatar Downloader!");
+console.log("Welcome to the GitHub Avatar Downloader!");
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var options = {
-//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//     headers: {
-//       "User-Agent": "request",
-//       "Authorization": "token " + GITHUB_TOKEN
-//     }
-//   };
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "request",
+      "Authorization": "token " + GITHUB_TOKEN
+    }
+  };
 
-//   request(options, function(err, res, body) {
-//     cb(err, JSON.parse(body));
-//   });
-// }
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   result.forEach(function(user) {
-//     console.log(user.avatar_url);
-//   })
-// });
-
-function downloadImageByURL(url, filePath) {
-  request.get("https://avatars2.githubusercontent.com/u/2741?v=3&s=466")
-    .pipe(fs.createWriteStream("./avatars/kvirani.jpg"));
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+  });
 }
 
-downloadImageByURL();
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  result.forEach(function(user) {
+    downloadImageByURL(user.avatar_url, "avatars/" + user.login + ".jpg");
+  })
+});
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .pipe(fs.createWriteStream(filePath));
+}
