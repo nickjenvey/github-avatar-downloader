0f518d9ec8b2a168a5a011651a60fce1ec873314
Nick <nbjenvey@gmail.com>
2019-06-04 00:05:13 +0000

    Fixed gitignore and cleaned up download_avatars

--- download_avatars.js
+++ download_avatars.js
@@ -12,28 +12,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
       "Authorization": "token " + GITHUB_TOKEN
     }
   };
-
   request(options, function(err, res, body) {
     cb(err, JSON.parse(body));
   });
 }
 
-var repoOwner = process.argv[2];
-var repoName = process.argv[3];
+var repoOwner = process.argv[2]; // Taking in argument for repoOwner
+var repoName = process.argv[3];  // Taking in argument for repoName
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
   if (process.argv.length !== 4) {
     console.log("Error! Not the specified amount of arguments given.");
   } else {
     result.forEach(function(user) {
-      downloadImageByURL(user.avatar_url, "avatars/" + user.login + ".jpg");
+      downloadImageByURL(user.avatar_url, "avatars/" + user.login + ".jpg"); //Callback to downloadImageByURL
       console.log("Downloading...")
-    })
-    console.log("Successfully downloaded the GitHub Avatars.")
+    });
+    console.log("Successfully downloaded the GitHub Avatars.");
   }
 });
 
 function downloadImageByURL(url, filePath) {
-  request.get(url)
-    .pipe(fs.createWriteStream(filePath));
+  request.get(url)                              // Grabbing the avatar_url URL's
+         .pipe(fs.createWriteStream(filePath)); // Saving the images to filePath
 }
